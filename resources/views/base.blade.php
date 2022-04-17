34bffe8d3eaafe38f2c2b416f13b09afe52ef19a
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2022-04-17 11:41:13 +0700

    3 Units Done
    Penyelesaian halaman index. Penambahan warna bootstrap (teal).

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     {{-- Bootstrap Link --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     {{-- Regular CSS Link --}}
     <link rel="stylesheet" href="{{ URL::asset('assets/styles/font.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('assets/styles/style.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('assets/styles/colors.css') }}">
 
     {{-- Fonts --}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
@@ -18,25 +21,22 @@
     {{-- Yield for title --}}
     <title>@yield('title') - Rujucare</title>
 </head>
-<body>
+
+<body class="d-flex flex-column min-vh-100">
     <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-        <div class="container-md">
+        <div class="container-xl">
             <a class="navbar-brand" href="{{ URL('/') }}"><img class="logo" src="{{ URL::asset('assets/images/logo.png') }}" alt="rujucare-logo" style="height: 36px"></a>
-            @hasSection('navbar')
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            @else
-            <div class="" id="navbarSupportedContent">
-            @endif
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     @yield('navbar')
                 </ul>
                 <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="width: 300px;">
-                    {{-- <button class="btn btn-outline-success me-2" type="submit">Search</button> --}}
-                    <button class="btn btn-success">Login</button>
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-teal me-2" type="submit">Search</button>
+                    <a class="btn btn-teal" href="{{ URL('/login') }}" role="button">Login</a>
                 </form>
             </div>
         </div>
@@ -45,37 +45,44 @@
     {{-- Yield for the actual content and sections --}}
     @yield('content')
 
-    <footer class="footer-dark">
-        <div class="container">
+    <footer class="mt-auto footer-dark">
+        <div class="container-xl">
             <div class="row">
-                <div class="col-8">
+                <div class="col-8 col-sm-6 col-md-8 col-lg-9">
                     <a class="navbar-brand" href="{{ URL('/') }}"><img class="logo" src="{{ URL::asset('assets/images/logo-dark.png') }}" alt="rujucare-logo"></a>
                 </div>
-                <div class="col-2">
+                <div class="col-4 col-sm-4 col-md-2 col-lg-2">
                     <h3>Search</h3>
                     <ul class="list-unstyled">
-                        <li><a href="{{ URL('/find/faskes') }}" class="text-muted text-reset text-decoration-none">Fasilitas Kesehatan</a></li>
-                        <li><a href="{{ URL('/find/spesialis') }}" class="text-muted text-reset text-decoration-none">Dokter Spesialis</a></li>
-                        <li></li>
+                        <li><a href="{{ URL('/find/faskes') }}" class="text-reset text-decoration-none">Fasilitas Kesehatan</a></li>
+                        <li><a href="{{ URL('/find/spesialis') }}" class="text-reset text-decoration-none">Dokter Spesialis</a></li>
                     </ul>
                 </div>
-                <div class="col-2 text-end">
-                    <ul></ul><button class="btn btn-success" type="button">Login</button>
+                <div class="col-2 col-lg-1 text-end">
+                    <a class="btn btn-teal" href="{{ URL('/login') }}" role="button">Login</a>
                 </div>
             </div>
             <hr>
             <div class="row">
-                <div class="col-8">
+                <div class="col-12 col-sm-6 col-md-8 col-lg-9">
                     <p class="copyright">© Rujucare, Inc. 2022</p>
                 </div>
-                <div class="col-4">
-                    <p>Follow us: </p>
+                <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><p>Follow us:</p></li>
+                        <li class="list-inline-item"><a class="list-inline-item me-1 text-reset" href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a class="list-inline-item me-1 text-reset" href="#"><i class="fab fa-facebook" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a class="list-inline-item me-1 text-reset" href="#"><i class="fab fa-discord" aria-hidden="true"></i></a></li>
+                        <li class="list-inline-item"><a class="list-inline-item me-1 text-reset" target="_blank" href="https://github.com/harisde03/INF206-2022-RI6-Rujucare"><i class="fab fa-github" aria-hidden="true"></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </footer>
 
     {{-- Bootstrap JavaScript Link --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
--- resources/views/rujucare/index.blade.php
+++ resources/views/rujucare/index.blade.php
@@ -6,33 +6,27 @@
 @section('title', 'Homepage')
 
 @section('content')
-    {{-- Working Area --}}
-    <section class="home_background">
-        <div class="home_white_background">
-            <div class="container position-absolute top-50 start-50 translate-middle">
-                <h1>Temukan Faskes Segera</h1>
-                <p>Rujucare dapat membantu anda menemukan fasilitas kesehatan untuk melakukan rujukan pasien dengan cepat</p>
-                <table>
-                    <td>
-                        <div class="card btn" style="padding: 24px 0; width: 320px;">
-                            <div class="card-body">
-                                <i class="fas fa-user-md" style="font-size: 64px;"></i>
-                                <br><br>
-                                <h4 class="card-title">Temukan Fasilitas Kesehatan</h4>
-                            </div>
-                        </div>
-                    </td>
-                    <td style="width: 5%;"></td>
-                    <td>
-                        <div class="card btn" style="padding: 24px 0; width: 320px;">
-                            <div class="card-body">
-                                <i class="fas fa-hospital-alt" style="font-size: 64px;"></i>
-                                <br><br>
-                                <h4 class="card-title">Temukan Spesialis</h4>
-                            </div>
-                        </div>
-                    </td>
-                </table>
+    <section class="home_background vh-100">
+        <div class="d-flex align-items-center" style="height: 100%; background-color: rgba(238, 238, 238, 0.75);">
+            <div class="container-xl">
+                <div class="row">
+                    <div class="col-lg-8 col-xl-7 col-xxl-6">
+                        <h1>Temukan Faskes Segera</h1>
+                        <p>Rujucare dapat membantu anda menemukan fasilitas kesehatan untuk melakukan rujukan pasien dengan cepat.</p>
+                    </div>
+                </div>
+                <div class="row text-center g-1">
+                    <div class="card col-12 col-md-5 col-lg-4 col-xxl-3 align-self-center" style="padding: 32px; margin: 16px 16px 0px 0px; border-radius: 30px;">
+                        <i class="fas fa-user-md" style="font-size: 64px;"></i><br>
+                        <h4>Temukan Spesialis</h4>
+                        <a href="{{ URL('/find/faskes') }}" class="stretched-link"></a>
+                    </div>
+                    <div class="card col-12 col-md-5 col-lg-4 col-xxl-3 align-self-center" style="padding: 32px; margin: 16px 16px 0px 0px; border-radius: 30px;">
+                        <i class="fas fa-hospital-alt" style="font-size: 64px;"></i><br>
+                        <h4>Temukan Fasilitas Kesehatan</h4>
+                        <a href="{{ URL('/find/spesialis') }}" class="stretched-link"></a>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
