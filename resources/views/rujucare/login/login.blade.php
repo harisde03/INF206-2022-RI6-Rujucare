690fc6aa4254c4ecac541836c3433e35fd84e5fd
maghfirahza10 <firah.za@mhs.unsyiah.ac.id>
2022-04-17 22:03:11 -0700

    commit

--- resources/views/rujucare/login/login.blade.php
+++ resources/views/rujucare/login/login.blade.php
@@ -5,6 +5,26 @@
 @section('title', 'Login')
 
 @section('content')
-    {{-- Working area --}}
-    <h1>Halaman Login</h1>
+<div class="container px-5">
+  <div class="row gx-6">
+    <div class="col">
+     <div class="p-3 border bg-light"><h1>Komunitas Rumah Sakit Terbesar Se-Indonesia</h1></div>
+    </div>
+    <div class="col">
+    <div class="row"><h3>Login Rumah Sakit</h3></div>
+    <div class="form-floating mb-3">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+    </div>
+        <div class="row">
+        <button type="button" class="btn btn-success">Login</button>
+    </div>
+        <div class="row">
+        <p>Belum Mendaftarkan Rumah Sakit? <a href="#">Ajukan Pendaftaran</a></p>
+    </div>
+    </div>
 @endsection
