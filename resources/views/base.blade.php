d1785e7b5dd4bc92fbffe068e7cea736c9c5a978
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2022-04-19 17:27:38 +0700

    Created the body of Informasi Umum page
    Awaiting for the sidebar from Informasi Ketersediaan page.

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,7 +10,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     {{-- Regular CSS Link --}}
-    <link rel="stylesheet" href="{{ URL::asset('assets/styles/font.css') }}">
+    {{-- <link rel="stylesheet" href="{{ URL::asset('assets/styles/font.css') }}"> --}}
     <link rel="stylesheet" href="{{ URL::asset('assets/styles/style.css') }}">
     <link rel="stylesheet" href="{{ URL::asset('assets/styles/colors.css') }}">
     <link rel="stylesheet" href="style.css">
--- resources/views/rujucare/admin/informasi-profil.blade.php
+++ resources/views/rujucare/admin/informasi-profil.blade.php
@@ -6,5 +6,59 @@
 
 @section('content')
     {{-- Working area --}}
-    <h1>Halaman Informasi Profil</h1>
+    <div class="container-xl">
+        <h2>Informasi Umum</h2>
+        <hr>
+        <form action="">
+            <div class="row">
+                <div class="col-7">
+                    <div class="">
+                        <h4>Nama Fasilitas Kesehatan</h4>
+                        <input type="text" class="form-control" placeholder="Nama Fasilitas Kesehatan">
+                        <p>Nama fasilitas akan ditampilkan pada halaman utama rumah sakit dan akan ditampilkan pada menu pencarian.</p>
+                    </div>
+                    <div class="">
+                        <h4>Email Publik</h4>
+                        <input type="text" class="form-control" placeholder="Email Publik">
+                        <p>Email akan ditampilkan sebagai tempat menghubungi fasilitas kesehatan.</p>
+                    </div>
+                    <div class="">
+                        <h4>Telepon Publik</h4>
+                        <input type="text" class="form-control" placeholder="Telepon Publik">
+                        <p>Nomor Telepon merupakan nomor yang dapat dihubungi untuk keadaanemergensi.</p>
+                    </div>
+                    <div class="">
+                        <h4>Deskripsi Fasilitas Kesehatan</h4>
+                        <textarea class="form-control" rows="5" placeholder="Deskripsi Fasilitas Kesehatan"></textarea>
+                        <p>Deskripsi hanya menampilkan informasi umum terkait fasilitas kesehatan.</p>
+                    </div>
+                    <div class="">
+                        <h4>Alaamt Fasilitas Kesehatan</h4>
+                        <textarea class="form-control" rows="5" placeholder="Alamat Fasilitas Kesehatan"></textarea>
+                        <p>Alamat fasilitas lengkap.</p>
+                    </div>
+                    <div class="">
+                        <h4>Tingkat Fasilitas Keshetan</h4>
+                        <select class="form-select">
+                            <option>Pilih Tingkat Fasilitas Kesehatan</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                        <p>Perubahan tingkat fasilitas kesehatan membutuhkan konfirmasi.</p>
+                    </div>
+                    <a href="#" class="btn btn-teal">Lakukan Perubahan</a>
+                </div>
+                <div class="col-5">
+                    <div class="">
+                        <h4>Foto Profil</h4>
+                        <img class="rounded-circle" src="{{ URL::asset('assets\images\contohRumahSakit.png') }}" alt="foto-profil" style="height: 200px; width: 200px;">
+                        <p>Foto Profil akan ditampilkan sebagai foto utama Fasilitas Kesehatan. Gunakan foto resolusi tinggi.</p>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 @endsection
