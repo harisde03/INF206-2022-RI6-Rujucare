94af1f28b3dcc8c9ea04eda57dbb95d58ccd2425
siapaituryan <ahmad_c20@mhs.unsyiah.ac.id>
2022-04-23 02:25:35 +0700

    find faskes (part 4)

--- resources/views/rujucare/find/faskes.blade.php
+++ resources/views/rujucare/find/faskes.blade.php
@@ -10,21 +10,22 @@
     th,
     td {
         border: 1px solid;
-        border-color: #C4C4C4;
+        border-color: #AAD9D3;
         padding: 8px
     }
 
     tr:nth-child(odd) {
-        background-color: #F8F8F8;
+        background-color: #E8F6F5;
     }
 
     th {
-        background-color: #CCCCCC;
-        text-align: left
+        background-color: #00ADB4;
+        text-align: left;
+        color: white
     }
 
     table {
-        width: 60%;
+        width: 65%;
         margin-left: auto;
         margin-right: auto;
         border-collapse: collapse
@@ -45,7 +46,7 @@
         background: transparent url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E") no-repeat 13px center;
     }
 
-    input[type="search"]::placeholder {
+    input.nosubmit[type="search"]::placeholder {
         color: #18A0FB;
         font-size: 20px;
     }
@@ -62,8 +63,8 @@
 <table>
     <tr>
         <th>Nama Fasilitas Kesehatan</th>
-        <th>Status Rumah Sakit</th>
-        <th>Rujukan Tersedia</th>
+        <th style="width:34.5%">Status Fasilitas Kesehatan</th>
+        <th style="width:27.75%">Kamar Tersedia</th>
     </tr>
     <tr>
         <td>a</td>
