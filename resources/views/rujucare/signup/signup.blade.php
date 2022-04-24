fe5c89ee378db613dc6f8764a990ed32124f6a19
Nadia Rimawati <nadia_wati@mhs.unsyiah.ac.id>
2022-04-25 00:17:54 +0700

    final signup

--- resources/views/rujucare/signup/signup.blade.php
+++ resources/views/rujucare/signup/signup.blade.php
@@ -5,44 +5,47 @@
 @section('title', 'Signup')
 
 @section('content')
-<div class="container px-5">
-  <div class="row gx-6">
-    <div class="col">
-    <div class="p-3 border bg-light, "><h1>Komunitas Rumah Sakit Terbesar Se-Indonesia</h1></div>
- </div>
-  <div class="col">
-    <div class="row"><h3>Daftar Rumah Sakit</h3></div>
-    <div class="form-floating mb-3">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Email Rumah Sakit</label>
-    </div>
-    <div class="form-floating mb-3">
-        <input type="nama rumah sakit" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPInput">Nama Rumah Sakit</label>
-    </div>
-    <div class="form-floating mb-3">
-        <input type="tingkat fasilitas kesehatan" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingInput">Tingkat Fasilitas Kesehatan</label>
-    </div>
-    <div class="form-floating mb-3">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPassword">Password</label>
-    </div>
-    <div class="form-floating mb-3">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPassword">Confirm Password</label>
-     </div>
-     </div>
-     <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Dengan menekan tombol anda telah sepenuhnya 
-            menyetujui persyaratan dan persetujuan dari layanan
-        </label>
-     </div>
-    <div class="row col-2">
-    <button type="button" class="btn btn-success">Daftar</button>
-    </div>
-    </div>
-    </div>
-    
+<section class="vh-90">
+        <div class="d-flex align-items-center h-100">
+            <div class="container-xl">
+                <div class="row">
+                    <div class="col d-flex align-items-center justify-content-center">
+                        <h1>Komunitas Fasilitas Kesehatan Terbesar Se-Indonesia</h1>
+                    </div>
+                    <div class="col border">
+                        <form action="">
+                            <div class="m-5">
+                                <div class="">
+                                    <h4>Daftar Fasilitas Kesehatan</h4>
+                                </div>
+                                <div class="mt-3">
+                                    <input type="text" class="form-control" placeholder="Email Fasilitas Kesehatan">
+                                </div>
+                                <div class="mt-3">
+                                    <input type="text" class="form-control" placeholder="Nama Fasilitas Kesehatan">
+                                </div>
+                                <div class="mt-3">
+                                    <input type="text" class="form-control" placeholder="Tingkat Fasilitas Kesehatan">
+                                </div>
+                                <div class="mt-3">
+                                    <input type="text" class="form-control" placeholder="Password">
+                                </div>
+                                <div class="mt-3">
+                                    <input type="text" class="form-control" placeholder="Konfirmasi Password">
+                                </div>
+                                <div class="mt-3">
+                                <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label"for="exampleCheck1">Dengan menekan tombol daftar anda telah sepenuhnya menyetujui  <a href="{{ URL('/signup/persetujuan') }}" class="text-teal text-decoration-none">persetujuan</a> dan <a href="{{ URL('/signup/persyaratan') }}" class="text-teal text-decoration-none">persyaratan</a> dari layanan kami</label>
+                                </div>
+                                <div class="mt-3">
+                                    <a href="/signup/upload-surat" class="btn btn-teal" role="button">Daftar</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
