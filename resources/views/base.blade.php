061be53fed90abc52e918a5c502d2e54e96ea3f6
Haris Daffa <79704418+harisde03@users.noreply.github.com>
2022-04-24 17:03:36 +0700

    Added Custom CSS for Bootstrap

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,10 +10,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     {{-- Regular CSS Link --}}
-    <!-- <link rel="stylesheet" href="{{ URL::asset('assets/styles/font.css') }}">  -->
+    {{-- <link rel="stylesheet" href="{{ URL::asset('assets/styles/font.css') }}"> --}}
     <link rel="stylesheet" href="{{ URL::asset('assets/styles/style.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('assets/styles/colors.css') }}">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ URL::asset('assets/styles/custom.css') }}">
+    {{-- <link rel="stylesheet" href="style.css"> --}}
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 
@@ -37,7 +37,7 @@
                 </ul>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-teal me-2" type="submit" style="color:white">Search</button>
+                    <button class="btn btn-outline-teal me-2" type="submit">Search</button>
                     <a class="btn btn-teal" href="{{ URL('/login') }}" role="button">Login</a>
                 </form>
             </div>
@@ -45,7 +45,9 @@
     </nav>
 
     {{-- Yield for the actual content and sections --}}
-    @yield('content')
+    <div id="content">
+        @yield('content')
+    </div>
 
     <footer class="mt-auto footer-dark">
         <div class="container-xl">
