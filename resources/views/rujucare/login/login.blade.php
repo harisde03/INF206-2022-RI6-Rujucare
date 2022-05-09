6f69bbf7211cc7a078e417f06dc7447b93de7f7e
Nadia Rimawati <nadia_wati@mhs.unsyiah.ac.id>
2022-05-09 10:56:53 +0700

    Login valid

--- resources/views/rujucare/login/login.blade.php
+++ resources/views/rujucare/login/login.blade.php
@@ -5,27 +5,62 @@
 @section('title', 'Login')
 
 @section('content')
-<div class="container px-5">
-  <div class="row gx-6">
-    <div class="col">
-     <div class="p-3 border bg-light" style="margin-top:60px;"><h1>Komunitas Rumah Sakit Terbesar Se-Indonesia</h1></div>
-    </div>
-    <div class="col" style="margin-top:20px;">
-    <div class="row"><h3>Login Rumah Sakit</h3></div>
+<section class="vh-90">
+    {{-- @dd(request()) --}}
+        <div class="d-flex align-items-center h-100">
+            <div class="container-xl">
 
-    <div class="form-floating mb-3" style="margin-bottom:10px;">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Email</label>
-    </div>
-    <div class="form-floating mb-3" style="margin-bottom:10px;">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPassword">Password</label>
-    </div>
-        <div class="row">
-        <button type="button" class="btn btn-success" style ="margin-top:20px;">Login</button>
-    </div>
-        <div class="row" style ="margin-top:20px;">
-        <p>Belum Mendaftarkan Rumah Sakit? <a href="{{URL('/signup')}}">Ajukan Pendaftaran</a></p>
-    </div>
-    </div>
+                <div class="row">
+                    @if(session()->has('success'))
+                        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                            {{session('success')}}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        @endif
+
+                        @if(session()->has('loginError'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {{session('loginError')}}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        @endif
+                    <div class="col d-flex align-items-center justify-content-center">
+                        <h1>Komunitas Fasilitas Kesehatan Terbesar Se-Indonesia</h1>
+                    </div>
+                    <div class="col border">
+                        <form action="/admin/informasi-ketersediaan" method="post">
+                            @csrf
+                            <div class="m-5">
+                                <div class="mb-2">
+                                    <h4>Login Fasilitas Kesehatan</h4>
+                                </div>
+                                <div class="mt-4">
+                                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email Fasilitas Kesehatan" autofocus required value={{old('email')}}>
+                                    @error('email')
+                                    <div class="invalid-feedback">
+                                       {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="mt-4">
+                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" required >
+                                    @error('password')
+                                    <div class="invalid-feedback">
+                                       {{$message}}
+                                    </div>
+                                    @enderror
+                                </div>
+                                <div class="mt-3">
+                                    <button href="{{ URL('/admin/informasi-ketersediaan') }}" type="button" class="btn btn-teal">Login</button>
+                                </div>
+                                <div class="mt-3">
+                                    <p>Belum Mendaftarkan Fasilitas Kesehatan? <a href="{{ URL('/signup') }}" class="text-decoration-none text-teal">Ajukan Pendaftaran</a></p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
