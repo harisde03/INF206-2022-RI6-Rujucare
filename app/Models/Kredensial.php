831f103bb3790c216b49590f889c3fb70dca457a
munyaja <mury220@mhs.unsyiah.ac.id>
2022-05-09 07:38:13 +0700

    update model kredensial

---
+++ app/Models/Kredensial.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\spesialis;
+use App\Models\ketersediaan;
+use App\Models\faskes;
+
+class Kredensial extends Model
+{
+    use HasFactory;
+    protected $guarded=['id'];
+
+    public function getSpesialis()
+    {
+        return $this->hasMany(Spesialis::class);
+    }
+
+    public function getKetersediaan()
+    {
+        return $this->hasOne(Ketersediaan::class);
+    }
+
+    public function faskes()
+    {
+        return $this->belongsTo(Faskes::class);
+    }
+}
